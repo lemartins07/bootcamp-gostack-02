7b07eb3132e0d841506d047ddd02b7a3d706c368
Leandro Martins <leandro.martins.85@gmail.com>
2020-03-29 22:35:07 -0300

    Avatar do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
   }
 
+  // define o relacionamento com o model File
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
