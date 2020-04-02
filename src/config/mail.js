e640f3fb3414c6b4ea15d130a2e6e909fd339d92
Leandro Martins <leandro.martins.85@gmail.com>
2020-04-02 12:26:31 -0300

    Configurando nodemailero

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4f1b05cc9f0535',
+    pass: 'bcb5847bdcfe74',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
