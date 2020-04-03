ea867a77ec707cee453c7ffed113a9ad7e2db7c6
Leandro Martins <leandro.martins.85@gmail.com>
2020-04-02 21:45:47 -0300

    Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     // habilita o uso de json
     this.server.use(express.json());
     // configura o server para fornecer arquivos estáticos, como as imagens.
@@ -24,6 +33,15 @@ class App {
   routes() {
     // importa as rotas da aplicação
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://60cd12d045fd4d539488723c2121973c@sentry.io/5187020',
+};
